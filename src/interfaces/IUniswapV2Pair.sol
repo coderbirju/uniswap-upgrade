e998c2753c3332f30beda446f673816de3c661d2
Arjun Raja Yogidas <yogidas.a@northeastern.edu>
2023-04-22 13:46:06 -0400

    reverting to new code

--- src/interfaces/IUniswapV2Pair.sol
+++ src/interfaces/IUniswapV2Pair.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import './IUniswapV2ERC20.sol';
+// import './IUniswapV2ERC20.sol';
 
-interface IUniswapV2Pair is IUniswapV2ERC20 {
+interface IUniswapV2Pair {
+
+    function initialize(address, address) external;
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function mint(address to) external returns (uint256 liquidity);
+    function burn(address to) external returns (uint256 amount0, uint256 amount1);
+    function swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data) external;
     // event Approval(address indexed owner, address indexed spender, uint256 value);
     // event Transfer(address indexed from, address indexed to, uint256 value);
 
@@ -24,33 +30,31 @@ interface IUniswapV2Pair is IUniswapV2ERC20 {
 
     // function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
 
-    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
-    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
-    event Swap(
-        address indexed sender,
-        uint256 amount0In,
-        uint256 amount1In,
-        uint256 amount0Out,
-        uint256 amount1Out,
-        address indexed to
-    );
-    event Sync(uint112 reserve0, uint112 reserve1);
+    // event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    // event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to);
+    // event Swap(
+    //     address indexed sender,
+    //     uint256 amount0In,
+    //     uint256 amount1In,
+    //     uint256 amount0Out,
+    //     uint256 amount1Out,
+    //     address indexed to
+    // );
+    // event Sync(uint112 reserve0, uint112 reserve1);
 
-    function MINIMUM_LIQUIDITY() external pure returns (uint256);
-    function factory() external view returns (address);
-    function token0() external view returns (address);
-    function token1() external view returns (address);
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    function price0CumulativeLast() external view returns (uint256);
-    function price1CumulativeLast() external view returns (uint256);
-    function kLast() external view returns (uint256);
+    // function MINIMUM_LIQUIDITY() external pure returns (uint256);
+    // function factory() external view returns (address);
+    // function token0() external view returns (address);
+    // function token1() external view returns (address);
+    
+    // function price0CumulativeLast() external view returns (uint256);
+    // function price1CumulativeLast() external view returns (uint256);
+    // function kLast() external view returns (uint256);
 
-    function mint(address to) external returns (uint256 liquidity);
-    function burn(address to) external returns (uint256 amount0, uint256 amount1);
-    function swap(uint256 amount0Out, uint256 amount1Out, address to, bytes calldata data) external;
-    function skim(address to) external;
-    function sync() external;
+    
+    
+    // function skim(address to) external;
+    // function sync() external;
 
-    function initialize(address, address) external;
 }
 
